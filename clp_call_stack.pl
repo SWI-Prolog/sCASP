52caab0009e2a01eb0ed2c695d5bf45fb0c24daa
Joaquin Arias <joaquin.arias@software.imdea.org>
2017-07-26 22:50:57 -0500

    The interpreter accumulate the call_stack

--- clp_call_stack.pl
+++ clp_call_stack.pl
@@ -1,22 +1,18 @@
-:- module(clp_call_stack,_).
-
+:- module(clp_call_stack, _).
 
 
 :- use_package(attr).
 
-
-:- op(700, xfx, [(~>), (<~)]).
-A ~> B :- get_attr_local(A, rules(B)).
-A <~ B :- put_attr_local(A, rules(B)).
-dump_rules([],[],[]).
-dump_rules([X|Xs],[_|Ns],[D|Ds]) :-
+:- op(700, xfx, [(~>), (<~), (<~>)]).
+A ~> Att :- get_attr_local(A, rules(Att)).
+A <~ Att :- put_attr_local(A, rules(Att)).
+dump_rules([],     [],     []).
+dump_rules([X|Xs], [_|Ns], [D|Ds]) :-
 	get_attr_local(X, rules(D)),
 	dump_rules(Xs, Ns, Ds).
-dump_rules([X|Xs],Ns,Ds) :-
-	\+ get_attr_local(X,rules(_)),
-	dump_rules(Xs,Ns,Ds).
-
-
+dump_rules([X|Xs], Ns, Ds) :-
+	\+ get_attr_local(X, rules(_)),
+	dump_rules(Xs, Ns, Ds).
 
 
 %% Auxiliar predicates %%
@@ -26,25 +22,33 @@ attr_portray_hook(Att, A) :- format(" ~w  .is  ~w ", [A, Att]).
 %% Auxiliar predicates %%
 
 
-
 %% TCLP interface %%
-%call_domain_projection(X, _) :- nl,print(call_domain(X)),nl.
 call_domain_projection(_, _).
 call_entail(_, _, _).
 call_store_projection(_, _, _).
 
-%answer_domain_projection(X,D) :- print(enter(X,D)), answer_domain_projection_(X,D), print(exit(X,D)),nl.
-answer_domain_projection(X,D) :- answer_domain_projection_(X,D).
-answer_domain_projection_([],[]).
-answer_domain_projection_([X|Xs], [D|Ds]) :- X ~> D, answer_domain_projection_(Xs,Ds).
-answer_domain_projection_([X|Xs], [ok|Ds]) :- \+ X ~> _, answer_domain_projection_(Xs,Ds).
+
+answer_domain_projection(X, D) :- answer_domain_projection_(X, D).
+answer_domain_projection_([],     []).
+answer_domain_projection_([X|Xs], [D|Ds]) :-
+	X ~> D, answer_domain_projection_(Xs, Ds).
+answer_domain_projection_([X|Xs], [ok|Ds]) :-
+	\+ X ~> _, answer_domain_projection_(Xs, Ds).
+
+answer_check_entail(_, [],       [],        1, _).
+answer_check_entail(_, [D1|D1s], [_D2|D2s], R, _) :-
+	D1 \= -(_, _), answer_check_entail(_, D1s, D2s, R, _).
 answer_check_entail(_, [-(_, N1)], [-(_, N2)], 1,  _) :- N1 >= N2.
 answer_check_entail(_, [-(_, N1)], [-(_, N2)], -1, _) :- N1 < N2.
+
 answer_store_projection(_, St, St).
 
-%apply_answer(Var,Ans) :- print(apply(Var,'\t',Ans)),nl,apply_answer_(Var,Ans).
-apply_answer(Var,Ans) :- apply_answer_(Var,Ans).
-apply_answer_([],  []).
-apply_answer_([X|Xs], [D|Ds]) :- D \= ok, X <~ D, apply_answer_(Xs,Ds).
-apply_answer_([_X|Xs], [ok|Ds]) :- apply_answer_(Xs,Ds).
+
+apply_answer(Var, Ans) :- apply_answer_(Var, Ans).
+apply_answer_([],     []).
+apply_answer_([X|Xs], [-(P, N)|Ds]) :- X <~ -(P, N), apply_answer_(Xs, Ds).
+apply_answer_([X|Xs], [s(P, N)|Ds]) :-
+	\+ X ~> _, X <~ s(P, N), apply_answer_(Xs, Ds).
+apply_answer_([X|Xs], [s(_, _)|Ds]) :-
+	X ~> _, apply_answer_(Xs, Ds).
 %% TCLP interface %%
--- tclp_asp.pl
+++ tclp_asp.pl
@@ -1,17 +1,31 @@
-%% USAGE:
-% ?- ?= [ab(X)].
-% { ab(sam) , penguin(sam) }
-% X = sam ? ;
-% { ab(john) , wounded_bird(john) }
-% X = john ? ;
-% no
-
 :- module(tclp_asp, _).
+
+%% USAGE:
+
+%% birds2_pr program
+% ?- ?= [flies(X)].
+% { model... }
+% X = tweety ? 
+
+% ?- ?? [flies(X)].
+% { call_stack ...}
+% { model ...}
+% X = tweety ?
+
+%% path/2 program
+% ?- ?= [path(X,Y)].
+% ...
+
+% ?- ?? [path(X,Y)].
+% ... see justification of results for path(1,4) in README ...
+
+%% It ALSO works for instantiated queries ?= [flies(tweety)]. and
+%% returns its model (if there is no model returns 'no')
 
 
 :- use_module(.(clp_call_stack)).
 :- op(700, xfx, [(~>), (<~)]).
-:- op(700, fx,  [(?=)]).
+:- op(700, fx,  [(?=), (??)]).
 
 :- use_module(.(clp_disequality_rt)).
 :- op(700, xfx, [(.\=.), (.=.)]).
@@ -20,15 +34,95 @@
 :- use_package(tabling).
 :- active_tclp.
 :- table query/2.
+:- table query_stack/4.
 :- table query2/1.
 
+%% simple interpreter only compute the justification but is not able
+%% to check the call_stack
 ?= X :-
-	query(X, P),
-	P ~> -(J, _),
-	print_model(J).
+	query(X, AttJ),
+	print_model(AttJ).
+
+%% this interpreter is based in the previous one and accumulate the
+%% call_stack in each iteration to check co-induction...
+??(A) :-
+	AttI <~ s([],0),
+	query_stack(A, AttI, AttO, AttJ),
+	print_stack(AttO),nl,
+	print_model(AttJ),nl.
 
 
-print_model([F|J]) :-
+%% Main predicate to compute the model and its justification.
+query([], Att) :-
+	Att <~ -([], 0).
+query([X|Xs], Att) :-
+	query_goal(X,  AttX), AttX ~> -(JX, NX),
+	query(Xs, AttXs), AttXs ~> -(JXs, NXs),
+	N is 1 + NX + NXs,
+	Att <~ -([X, JX|JXs], N).
+query_goal(X, Att) :-
+	predicate(X),
+	pr_rule(X, Body),
+	query(Body, Att).
+query_goal(X, Att) :-
+	\+ predicate(X),
+	X \= [], X \= [_|_],
+	q_exec(X),
+	Att <~ -([], 0).
+
+
+%% Main predicate to compute the model, its justification, and the
+%% call_stack in each iteration.
+query_stack([], AttI, AttI, AttJ) :-
+	AttJ <~ -([],0).
+query_stack([X|Xs], AttI, AttO, AttJ) :-
+ 	AttI ~> s(I,NI),
+%% 	format('Calling ~w \t with stack = ~w', [X, I]), nl,
+	NI1 is NI + 1,
+	AttI1 <~ s([X|I],NI1),
+	query_stack_goal(X, AttI1, AttO1, AttJx), AttJx ~> -(JX,NX),
+	query_stack(Xs, AttO1, AttO, AttJxs), AttJxs ~> -(JXs, NXs),
+	N is 1 + NX + NXs,
+	AttJ <~ -([X,JX|JXs],N).
+
+%% It is not tabled to execute the sub-goals and produce the
+%% side-effects
+query_stack_goal(X,  AttI, AttO, AttJ) :-
+	predicate(X),
+	pr_rule(X, Body), 
+	query_stack(Body, AttI, AttO, AttJ).
+query_stack_goal(X, AttI, AttI, AttJ) :-
+	\+ predicate(X),
+	X \= [], X \= [_|_],
+	q_exec(X),
+	AttJ <~ -([],0).
+
+
+%% Check if the goal X is a user defined predicate
+predicate(X) :-
+	X =.. [Name|ArgX],
+	pr_rule(R,_),
+	R =.. [Name|ArgR],
+	length(ArgX,N),
+	length(ArgR,N).
+
+%% Execute the non user define predicates using Prolog
+q_exec(A .\=. B) :-
+	(
+	    call(A .\=. B) ->
+%%	    print('OK'),nl,
+	    true
+	;
+	    print('OK: Disequality fails checking:  '), print(A .\=. B), nl,
+	    fail ).
+q_exec(X) :-
+	X \= .\=.(_, _),
+	call(X).
+
+
+%% The model is obtained from the justification tree.
+print_model(AttJ) :-
+	AttJ ~> -([F|J], _),
 	nl,
 	print('{ '),
 	print(F),
@@ -46,39 +140,25 @@ print_model_([X, Y|Xs]) :-
 	print(X),
 	print_model_([Y|Xs]).
 
+%% The stack is generated adding the last calls in the head (to avoid
+%% the use of append/3). To print the stack, it is reversed.
 
-
-query([], Att) :-
-	Att <~ -([], 0).
-query([X|Xs], Att) :-
-	query(X, AttX), AttX ~> -(JX, NX),
-	query(Xs, AttXs), AttXs ~> -(JXs, NXs),
-	N is 1 + NX + NXs,
-	Att <~ -([X, JX|JXs], N).
-query(X, Att) :-
-	pr_rule(X, Body),
-	query(Body, Att).
-query(X, Att) :-
-	\+ pr_rule(X,_),
-	X \= [], X \= [_|_],
-	q_exec(X),
-	Att <~ -([],0).
-
-q_exec(A .\=. B) :- 
-	(
-	    call(A .\=. B) ->
-	    true
-	;
-	    print('OK: Disequality fails checking:  '), print(A .\=. B), nl, fail
-	).
-q_exec(X) :-
-	X \= .\=.(_, _),
-	call(X).
+%% NOTE that a model could be generated during the search with some
+%% calls in the stack which are not present in the model (e.g. the
+%% model of path(1,4) for the path/2 program - more details in the
+%% file README)
+print_stack(AttS) :-
+	AttS ~> s(Stack, _),
+	reverse(Stack,RStack),
+	nl,
+	print('{ '),
+	print(RStack),
+	print(' }'),nl.
 
 
 
 
-
+%% Initial interpreters...
 query2([]).
 query2([X|Xs]) :-
 	query2(Xs),
@@ -90,7 +170,7 @@ query2(X) :-
 
 %:- table query3/3.
 query3([X|Xs], I, O) :-
-	format('llama ~w \t with stack = ~w', [X,I]), nl,
+	format('Calling ~w \t with stack = ~w', [X, I]), nl,
 	query3(X,  [X|I], O1),
 	query3(Xs, O1,    O).
 query3([], I, I) :- !.
@@ -102,27 +182,27 @@ query3(X,  I, O) :-
 
 
 
-%% A program in prolog $$
-path(A, B, [A|Ls]) :-
-	edge(A, Z),
-	path(Z, B, Ls).
+%% A program in Prolog $$
+% path(A, B, [A|Ls]) :-
+% 	edge(A, Z),
+% 	path(Z, B, Ls).
 
-path(A, B, [A, B]) :-
-	edge(A, B).
+% path(A, B, [A, B]) :-
+% 	edge(A, B).
 
-edge(1, 2).
-edge(2, 3).
-edge(2, 4).
+% edge(1, 2).
+% edge(2, 3).
+% edge(2, 4).
 
 
 %% A program translated in list format %%
-% pr_rule(path(A, B), [edge(A, B)]).
-% pr_rule(path(A, B), [edge(A, Z), path(Z, B)]).
+pr_rule(path(A, B), [edge(A, Z), path(Z, B)]).
+pr_rule(path(A, B), [edge(A, B)]).
 
-% pr_rule(edge(1, 2), []).
-% pr_rule(edge(2, 3), []).
-% % pr_rule(edge(1, 1), []).
-% % pr_rule(edge(2, 4), []).
+pr_rule(edge(2, 1), []).
+pr_rule(edge(1, 2), []).
+pr_rule(edge(1, 1), []).
+pr_rule(edge(2, 4), []).
 
 
 :- include('pr/birds2_pr.pl').
