b51324427b83831259981e1fd0661bc923b4fc89
Manuel Carro <manuel.carro@imdea.org>
2018-02-17 22:49:15 +0100

    Comments

--- misc_programs/hanoi_for_simple_minds.pl
+++ misc_programs/hanoi_for_simple_minds.pl
@@ -1,4 +1,29 @@
+/*
+Dumb (but likely correct) Towers of Hanoi
 
+This is a simple version of the Towers of Hanoi which:
+
+- Defines the ToH as a (blind) planning: it does any moves it can do
+  as long as there are discs in the pegs.
+
+- Does not tell you the order of the moves, only if there is a solution.
+
+- Defines global constraints to remove the incorrect states.
+
+- I cannot (as-is) be executed in ASP as it uses lists of size in
+  principle unknown, but we know that they are bounded by the number
+  of discs.
+
+However:
+
+- As there can be repeated movements without upper bound on its
+  number, it can just loop forever --> s(CASP) should be able to catch
+  that.
+
+- As the constraints on the bad movements are caught when the whole
+  model is generated, it should be in any case hugely inefficient.
+
+*/
 
 hanoi([], [], _).
 hanoi(A, B, C):-
