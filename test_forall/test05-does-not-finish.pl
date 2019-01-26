ce758aa5cb7642f00be9033d96eb23895ca3a777
Manuel Carro <manuel.carro@imdea.org>
2019-01-26 09:18:00 +0100

    This test should finish (it has no recursion) but it does not

---
+++ test_forall/test05-does-not-finish.pl
@@ -0,0 +1,14 @@
+
+
+p(X, Y, Z):- all_pos(X, Y), all_pos(Y, Z).
+
+% When the above works, try the one below
+%p(X, Y, Z):- all_pos(X, Y), all_pos(Y, Z).  % , all_pos(X, Z).
+
+all_pos(A, B):- A .>. B.
+all_pos(A, B):- A .=<. B.
+
+q:- not p(X, Y, Z).
+
+
+?-  not q. %% Should succeed
