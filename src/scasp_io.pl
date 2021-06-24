9604ef352d1699fe4148ad7f49c21cf1ea7576cb
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-06-24 16:53:23 +0200

    make --help work

--- src/scasp_io.pl
+++ src/scasp_io.pl
@@ -22,9 +22,12 @@
     answer_counter/1,
     print_html/3
     ]).
+:- if(current_prolog_flag(version_data, swi(_,_,_,_))).
 :- expects_dialect(ciao).
 :- style_check(-singleton).
 :- op(900, fy, user:not).
+goal_expansion(abort, halt).
+:- endif.
 
 %% ------------------------------------------------------------- %%
 :- use_package(assertions).
@@ -1073,57 +1076,57 @@ set(Option, Value) :-
     assert(current_option(Option,Value)).
 
 s_help :-
-    display('Usage: scasp [options] InputFile(s)\n\n'),
-    display('s(CASP) computes stable models of predicate normal logic programs with contraints\n'),
-    display('  using a top-down evaluation algorihtm.\n'),
-    display('Command-line switches are case-sensitive!\n\n'),
-    display('General Options:\n\n'),
-    display('  -h, -?, --help        Print this help message and terminate.\n'),
-    display('  --help_all            Print extended help.\n'),
-    display('  -i, --interactive     Run in interactive mode (REP loop).\n'),
-    display('  -a, --auto            Run in batch mode (no user interaction).\n'),
-    display('  -sN, -nN              Compute N answer sets, where N >= 0. N = 0 means ''all''.\n'),
-    display('  -c, --compiled        Load compiled files (e.g. extracted using --code).\n'),
-    display('  -d, --plaindual       Generate dual program with single-goal clauses\n'),
-    display('                        (for propositional programs).\n'),
-    display('  -r[=d]                Output rational numbers as real numbers.\n'),
-    display('                        [d] determines precision. Defaults to d = 5.\n'),
-    display('\n'),
-    display('  --code                Print program with dual clauses and exit.\n'),
-    display('  --tree                Print justification tree for each answer (if any).\n'),
-    display('\n'),
-    display('  --plain               Output code / justification tree as literals (default).\n'),
-    display('  --human               Output code / justification tree in natural language.\n'),
-    display('\n'),
-    display('  --long                Output long version of justification.\n'),
-    display('  --mid                 Output mid-sized version of justification (default) .\n'),
-    display('  --short               Short version of justification.\n'),
-    display('\n'),
-    display('  --pos                 Only display the selected literals in the justification.\n'),
-    display('  --neg                 Add the negated literals in the justification (default).\n'),
-    display('\n'),
-    display('  --html[=name]         Generate HTML file for the justification. [name]:\n'),
-    display('                        use \'name.html\'. Default: first InputFile name.\n'),
-    display('\n'),
-    display('  -v, --verbose         Enable verbose progress messages.\n'),
-    display('  -f, --tracefails      Trace user-predicate failures.\n'),
-    display('  --update              Automatically update s(CASP).\n'),
-    display('  --version             Output the current version of s(CASP)\n'),
-    display('\n'),
-    display('  --all_c_forall        Exhaustive evaluation of c_forall/2.\n'),
-    display('  --prev_forall         Deprecated evaluation of forall/2.\n'),
-    display('\n').
+    format('Usage: scasp [options] InputFile(s)\n\n'),
+    format('s(CASP) computes stable models of predicate normal logic programs with contraints\n'),
+    format('  using a top-down evaluation algorihtm.\n'),
+    format('Command-line switches are case-sensitive!\n\n'),
+    format('General Options:\n\n'),
+    format('  -h, -?, --help        Print this help message and terminate.\n'),
+    format('  --help_all            Print extended help.\n'),
+    format('  -i, --interactive     Run in interactive mode (REP loop).\n'),
+    format('  -a, --auto            Run in batch mode (no user interaction).\n'),
+    format('  -sN, -nN              Compute N answer sets, where N >= 0. N = 0 means ''all''.\n'),
+    format('  -c, --compiled        Load compiled files (e.g. extracted using --code).\n'),
+    format('  -d, --plaindual       Generate dual program with single-goal clauses\n'),
+    format('                        (for propositional programs).\n'),
+    format('  -r[=d]                Output rational numbers as real numbers.\n'),
+    format('                        [d] determines precision. Defaults to d = 5.\n'),
+    format('\n'),
+    format('  --code                Print program with dual clauses and exit.\n'),
+    format('  --tree                Print justification tree for each answer (if any).\n'),
+    format('\n'),
+    format('  --plain               Output code / justification tree as literals (default).\n'),
+    format('  --human               Output code / justification tree in natural language.\n'),
+    format('\n'),
+    format('  --long                Output long version of justification.\n'),
+    format('  --mid                 Output mid-sized version of justification (default) .\n'),
+    format('  --short               Short version of justification.\n'),
+    format('\n'),
+    format('  --pos                 Only format the selected literals in the justification.\n'),
+    format('  --neg                 Add the negated literals in the justification (default).\n'),
+    format('\n'),
+    format('  --html[=name]         Generate HTML file for the justification. [name]:\n'),
+    format('                        use \'name.html\'. Default: first InputFile name.\n'),
+    format('\n'),
+    format('  -v, --verbose         Enable verbose progress messages.\n'),
+    format('  -f, --tracefails      Trace user-predicate failures.\n'),
+    format('  --update              Automatically update s(CASP).\n'),
+    format('  --version             Output the current version of s(CASP)\n'),
+    format('\n'),
+    format('  --all_c_forall        Exhaustive evaluation of c_forall/2.\n'),
+    format('  --prev_forall         Deprecated evaluation of forall/2.\n'),
+    format('\n').
 
 help_all :-
     help,
-    display('  --no_olon             Do not compile olon rules (for debugging purposes).\n'),
-    display('  --no_nmr              Do not compile NMR checks (for debugging purposes).\n'),
-    display('  -w, --warning         Enable warning messages (failures in variant loops / disequality).\n'),
-    display('  --variant             Do not fail in the presence of variant loops.\n'),
-    display('\n'),
-    display('  -m, --minimal         Collect only the minimal models (TABLING required).\n'),
-    display('  --raw                 Sort the clauses as s(ASP) does (use with --code).\n'),
-    display('\n').
+    format('  --no_olon             Do not compile olon rules (for debugging purposes).\n'),
+    format('  --no_nmr              Do not compile NMR checks (for debugging purposes).\n'),
+    format('  -w, --warning         Enable warning messages (failures in variant loops / disequality).\n'),
+    format('  --variant             Do not fail in the presence of variant loops.\n'),
+    format('\n'),
+    format('  -m, --minimal         Collect only the minimal models (TABLING required).\n'),
+    format('  --raw                 Sort the clauses as s(ASP) does (use with --code).\n'),
+    format('\n').
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
