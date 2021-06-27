a1607600e038f06c25e55598ecb5916c685386b7
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-06-27 14:01:00 +0200

    Constructive (dis)equality based on unifiable/3.

--- src/clp_disequality_rt.pl
+++ src/clp_disequality_rt.pl
@@ -52,8 +52,23 @@ of Normal Logic Programs Without Grounding} by @em{Marple et al. 2017}.
 %% with a non- variable value will succeed if the non-variable value
 %% does not constructively unify with any element in the variable’s
 %% prohibited value list.
+
+:- if(\+((dif(A,b), unifiable(a(A), a(b), _)))).
+:- format(user_error, 'ERROR: unifiable/3 validates constraints~n', []),
+   halt(1).
+:- endif.
+
+
 .=.(A,B) :-
-    var(A),
+    unifiable(A,B,U),
+    unify(U).
+
+unify([]).
+unify([Var=Value|T]) :-
+    unify2(Var, Value),
+    unify(T).
+
+unify2(A, B) :-
     nonvar(B), !,
     (   get_neg_var(A,NegListA)
     ->  not_unify(B, NegListA),
@@ -61,56 +76,21 @@ of Normal Logic Programs Without Grounding} by @em{Marple et al. 2017}.
     ;   true
     ),
     A = B.
-
-.=.(B,A) :-
-    var(A),
-    nonvar(B), !,
-    (   get_neg_var(A,NegListA)
-    ->  not_unify(B, NegListA),
-        clean(A)
-    ;   true
-    ),
-    A = B.
-
 %% - Constructive unification of two negatively constrained variables
 %% will always succeed, setting their shared prohibited value list to
 %% the union of their original lists.
-.=.(A,B) :-
+unify2(A, B) :-
     get_neg_var_or_empty(A,NegListA),
     get_neg_var_or_empty(B,NegListB), !,
     ord_union(NegListA,NegListB,NegList),
     update(A,NegList),
     clean(B),
     B = A.
-
-%% - Constructive unification of two compound terms is performed
-%% recursively: first, the functors and arities are tested, then each
-%% pair of corresponding arguments is constructively unified.
-
-%% particular case for lists (they are also struct)
-.=.(A,B) :-
-    compound(A),
-    compound(B),
-    !,
-    functor(A, Name, Arity),
-    functor(B, Name, Arity),
-    cunif(1, Arity, A, B).
-
 %% - In cases where neither argument contains a negatively constrained
 %% variable, the result is identical to that of traditional
 %% unification.
-.=.(A,B) :-
+unify2(A,B) :-
     A = B.
-
-cunif(I, Arity, T1, T2) :-
-    I =< Arity,
-    !,
-    arg(I, T1, A1),
-    arg(I, T2, A2),
-    A1 .=. A2,
-    I2 is I+1,
-    cunif(I2, Arity, T1, T2).
-cunif(_, _, _, _).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -118,124 +98,34 @@ cunif(_, _, _, _).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 .\=.(A,B) :-
-    ground(A),
-    ground(B), !,
-    A \= B.
+    (   unifiable(A,B,U)
+    ->  not_unify(U)
+    ;   true
+    ).
 
-:- use_module(clp_clpq).
-.\=.(A,B) :-
-    is_clpq_var(A), !,
-    disequality_clpq(A,B).
-.\=.(A,B) :-
-    is_clpq_var(B), !,
-    disequality_clpq(B,A).
-% .\=.(A,B) :-
-%       var(A),
-%       num(B),
-%       disequality_clpq(A,B).
-% .\=.(A,B) :-
-%       var(B),
-%       num(A),
-%       disequality_clpq(B,A).
-
-
-%% - Constructive disunification of a negatively constrained variable
-%% and a non- variable value will always succeed, adding the
-%% "ground" value to the variable’s prohibited value list.
-.\=.(A,B) :-
-    neg_var(A,NegListA),
-    ground(B), !,
-%       assert(disunify(A,B)),
-    insert(NegListA,B,NegList),
-    update(A,NegList).
-
-.\=.(B,A) :-
-    neg_var(A,NegListA),
-    ground(B), !,
-%       assert(disunify(A,B)),
-    insert(NegListA,B,NegList),
-    update(A,NegList).
-
+not_unify([]).
+not_unify([Var=Value|T]) :-
+    not_unify2(Var, Value),
+    not_unify(T).
 
 %% - in accordance with the restrictions given in Section 3.1.5,
 %% constructive disunification of two non ground variables will
 %% produce an error
-.\=.(A,B) :-
-    var(A), var(B),
-    \+ ground(A),
-    \+ ground(B), !,
-%       send_silent_signal(error),
-%       format('ERROR: disunification expect at least one argument to be ground, got:\n~p\n~p\n',[A,B]),
+not_unify2(_,B) :-
+    var(B),
+    !,
     fail.
-
-
-%% - Constructive disunification of two compound terms is performed by
-%% first test- ing functors and arities. If either of these does not
-%% match, the operation succeeds deterministically. Otherwise, the
-%% pairs of corresponding arguments are handled
-%% recursively. Non-deterministic success occurs as soon as the
-%% operation succeeds for a pair of arguments, with subsequent pairs
-%% tested upon backtracking.
-
-%% particular case for lists (they are also struct)
-% .\=.([],[]) :- !, fail.
-% .\=.(ListA, ListB) :-
-%       \+ var(ListA),
-%       \+ var(ListB),
-%       ListA = [A|As],
-%       ListB = [B|Bs], !,
-%       format('disequality ~p \= ~p\n',[ListA,ListB]),
-%       (
-%           format('\tdisequality ~p \= ~p\n',[A,B]),
-%           A .\=. B
-%       ;
-%           format('\tdisequality ~p \= ~p\n',[A,B]),
-%           As .\=. Bs
-%       ).
-
-% .\=.(A,B) :-
-%       \+ var(A),
-%       \+ var(B),
-%       struct(A),
-%       struct(B), !,
-%       A =.. [NameA | As],
-%       B =.. [NameB | Bs],
-%       (
-%           NameA \= NameB ->
-%           true
-%       ;
-%           As .\=. Bs
-%       ).
-
-.\=.(A,B) :-
-    \+ var(A),
-    \+ var(B),
-    struct(A),
-    struct(B), !,
-%       format('check_dual(~p \= ~p)\n',[A,B]),
-    (
-        unifiable(A,B,Unifier) ->
-%           format('check_dual(~p)\n',[Unifier]),
-        check_dual(Unifier)
-    ;
-        true
-    ).
-
-% .\=.(A,B) :-
-%       print('vars'),
-%       disequality_clpq(A,B).
-
-%% - In cases where neither argument contains a negatively constrained
-%% variable, the result is identical to that of traditional
-%% disunification.
-.\=.(A,B) :-
-    \+ var(A), \+ var(B),
-    A \= B.
-
-check_dual([A=B|_]) :-
-    A .\=. B.
-check_dual([_|Ds]) :-
-    check_dual(Ds).
+not_unify2(A, B) :-
+    is_clpq_var(A), !,
+    disequality_clpq(A,B).
+%% - Constructive disunification of a negatively constrained variable
+%% and a non- variable value will always succeed, adding the
+%% "ground" value to the variable’s prohibited value list.
+not_unify2(A,B) :-
+    neg_var(A,NegListA),
+    ground(B), !,
+    insert(NegListA,B,NegList),
+    update(A,NegList).
 
 loop_list_disequality([A|As],[B|Bs]) :-
     (
