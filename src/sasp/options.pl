135a087f33ccc8573c6888ba2f5c51f73c579d7c
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-07-12 15:45:06 +0200

    Style review for options.pl

--- src/sasp/options.pl
+++ src/sasp/options.pl
@@ -1,27 +1,7 @@
-:- module(options, [
-                    user_option/1,
-                    user_option/2,
-                    set_default_options/0,
-                    set_user_option/1,
-                    set_user_option/2,
-                    option_cleanup/0,
-                    set_stack_sizes/0
-                 ]).
-
-/** <module> Assertion, retraction and testing of options.
-
-Predicates related to managing options that alter runtime behavior.
-
-
-@author Kyle Marple
-@version 20170127
-@license BSD-3
-*/
-
 /*
 * Copyright (c) 2016, University of Texas at Dallas
 * All rights reserved.
-*  
+*
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
 *     * Redistributions of source code must retain the above copyright
@@ -32,7 +12,7 @@ Predicates related to managing options that alter runtime behavior.
 *     * Neither the name of the University of Texas at Dallas nor the
 *       names of its contributors may be used to endorse or promote products
 *       derived from this software without specific prior written permission.
-*  
+*
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -45,42 +25,70 @@ Predicates related to managing options that alter runtime behavior.
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+:- module(options,
+          [ user_option/1,
+            user_option/2,
+            set_default_options/0,
+            set_user_option/1,
+            set_user_option/2,
+            option_cleanup/0,
+            set_stack_sizes/0
+          ]).
+
+/** <module> Assertion, retraction and testing of options.
+
+Predicates related to managing options that alter runtime behavior.
+
+@author Kyle Marple
+@version 20170127
+@license BSD-3
+*/
+
 :- use_module(config). % for default options
 
-%! user_option(+Mode:atom) is det
-% Asserted for options that are specified by the user via command-line
-% arguments. Where applicable, these can override settings hard-coded into a
-% program, such as the number of answer sets to compute. If not specified by the
-% user, the hard-coded value would normally override the default.
+%!  user_option(+Mode:atom) is det
 %
-% @param Mode The option specified by the user.
+%   Asserted for options that are specified by the user via command-line
+%   arguments. Where applicable, these can  override settings hard-coded
+%   into a program, such as the number of answer sets to compute. If not
+%   specified by the user, the hard-coded  value would normally override
+%   the default.
+%
+%   @arg Mode The option specified by the user.
 
-%! user_option(+Mode:atom, +Value:compound) is det
-% Asserted for applicable options by set_user_option/2. Associates each option with
-% a binary flag indicating whether or not the option is enabled. Only one fact
-% should be asserted per Mode (ensured by set_user_option/2).
+%!  user_option(+Mode:atom, +Value:compound) is det
 %
-% @param Mode An atom identifying the option.
-% @param Value The value for the option.
+%   Asserted for applicable  options   by  set_user_option/2. Associates
+%   each option with a binary flag indicating  whether or not the option
+%   is enabled. Only one fact should be   asserted  per Mode (ensured by
+%   set_user_option/2).
+%
+%   @arg Mode An atom identifying the option.
+%   @arg Value The value for the option.
+
 :- dynamic
     user_option/1,
     user_option/2.
 
-%! set_default_options
-% Get a list of all config:default_option/2 facts and set the options
-% accordingly.
+%!  set_default_options
+%
+%   Get a list of all config:default_option/2  facts and set the options
+%   accordingly.
+
 set_default_options :-
     findall(-(X, Y), default_option(X, Y), Defaults),
     set_default_options2(Defaults).
 
-%! set_default_options2(+Options:list)
-% For each option / flag pair, call set_user_option/2 after checking that no
-% other entry for the same option exists.
+%!  set_default_options2(+Options:list)
 %
-% @param Options List of options to set.
+%   For each option / flag pair,   call set_user_option/2 after checking
+%   that no other entry for the same option exists.
+%
+%   @arg Options List of options to set.
+
 set_default_options2([X | T]) :-
     X = -(A, B),
-    \+user_option(A, _), % not overridden by user
+    \+ user_option(A, _), % not overridden by user
     set_user_option(A, B),
     !,
     set_default_options2(T).
@@ -91,38 +99,44 @@ set_default_options2([X | T]) :-
     set_default_options2(T).
 set_default_options2([]).
 
-%! set_user_option(+Mode:atom) is det
-% Assert user_option(Mode).
+%!  set_user_option(+Mode:atom) is det
 %
-% @param Mode The option specified by the user.
+%   Assert user_option(Mode).
+%
+%   @arg Mode The option specified by the user.
+
 set_user_option(Mode) :-
     assertz(user_option(Mode)).
 
-%! set_user_option(+Mode:atom, +Value:compound)
-% Assert an option with a given value.
+%!  set_user_option(+Mode:atom, +Value:compound)
 %
-% @param Mode The option to assert.
-% @param Value The value for the option.
+%   Assert an option with a given value.
+%
+%   @arg Mode The option to assert.
+%   @arg Value The value for the option.
+
 set_user_option(M, X) :-
     retractall(user_option(M, _)),
     assertz(user_option(M, X)),
     !.
 
-%! option_cleanup
-% Cleanup (retract) all user_option/1 and user_option/2 assertions.
+%!  option_cleanup
+%
+%   Cleanup (retract) all user_option/1 and user_option/2 assertions.
+
 option_cleanup :-
     retractall(user_option(_, _)),
     retractall(user_option(_)),
     !.
 
-%! set_stack_sizes
-% Increase the default stack sizes to the value indicated by
-% config:stack_size/1. For 32-bit systems SWI should ignore this command. For
-% 64-bit systems, a limit larger than the system allows will be automatically
-% reduced to the maximum allowed size.
+%!  set_stack_sizes
+%
+%   Increase  the  default  stack  sizes  to   the  value  indicated  by
+%   config:stack_size/1. For 32-bit  systems  SWI   should  ignore  this
+%   command. For 64-bit systems, a limit   larger than the system allows
+%   will be automatically reduced to the maximum allowed size.
+
 set_stack_sizes :-
-    stack_size(_X),
-    % set_prolog_stack(global, limit(X)),
-    % set_prolog_stack(local, limit(X)),
-    % set_prolog_stack(trail, limit(X)),
-    !.
+    stack_size(BytesExpr),
+    Bytes is BytesExpr,
+    set_prolog_flag(stack_limit, Bytes).
