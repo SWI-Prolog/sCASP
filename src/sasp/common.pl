b0767c876901c4b624f6095ae9a613fac9a22b71
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-07-12 15:24:06 +0200

    Style review for sasp/common.pl

--- src/sasp/common.pl
+++ src/sasp/common.pl
@@ -56,50 +56,58 @@ Common and utility predicates that may be called from multiple locations.
 */
 
 :- use_module(library(lists)).
-:- use_module(debug).
 :- use_module(options).
 :- use_module(variables).
 :- use_module(program, [has_prefix/2]).
 
-%! predicate(?PredicateStruct:compound, ?Name:atom, ?Args:list) is det
-% Convert a predicate struct to its components, or vice-versa. Ensure this
-% doesn't succeed for operators or not(_).
+%!  predicate(?PredicateStruct:compound, ?Name:atom, ?Args:list) is det
 %
-% @arg PredicateStruct Predicate sturct.
-% @arg Name Predicate name, in name/arity format.
-% @arg Args List of predicate args.
+%   Convert a predicate struct to its  components, or vice-versa. Ensure
+%   this doesn't succeed for operators or not(_).
+%
+%   @arg  PredicateStruct Predicate sturct.
+%   @arg  Name Predicate name, in name/arity format.
+%   @arg  Args List of predicate args.
+
 predicate(Predicate, Name, Args) :-
     Predicate =.. [Name | Args],
-    \+operator(Name, _, _),
-    Name \= 'not',
+    \+ operator(Name, _, _),
+    Name \= not,
     !.
 
-%! c_rule(?Rule:compound, ?Head:compound, ?Body:list) is det
-% Convert a rule structure into its head and body, or vice-versa. Note that if
-% an ID has been attached, it will be paired with the head as
-% =|Head = -(RealHead, ID)|=. This can be taken advantage of if the head and ID
-% are simply being copied, but should be used with care.
+%!  c_rule(?Rule:compound, ?Head:compound, ?Body:list) is det
 %
-% @arg Rule Rule struct.
-% @arg Head Rule head.
-% @arg Body Rule body.
+%   Convert a rule structure into its head and body, or vice-versa. Note
+%   that if an ID has been attached, it  will be paired with the head as
+%   `Head = -(RealHead, ID)`. This can be taken advantage of if the head
+%   and ID are simply being copied, but should be used with care.
+%
+%   @arg  Rule Rule struct.
+%   @arg  Head Rule head.
+%   @arg  Body Rule body.
+
 c_rule(-(H, B), H, B).
 
-%! rule(?Rule:compound, ?Head:compound, ?ID:int, ?Body:list) is det
-% Convert a rule structure with an id into its head, ID and body, or vice-versa.
+%!  rule(?Rule:compound, ?Head:compound, ?ID:int, ?Body:list) is det
 %
-% @arg Rule Rule struct.
-% @arg Head Rule head.
-% @arg ID Rule ID.
-% @arg Body Rule body.
+%   Convert a rule structure with an id into   its head, ID and body, or
+%   vice-versa.
+%
+%   @arg  Rule Rule struct.
+%   @arg  Head Rule head.
+%   @arg  ID Rule ID.
+%   @arg  Body Rule body.
+
 rule(-(-(H, I), B), H, I, B).
 
-%! negate_functor(+Functor:compound, -NegFunctor:compound) is det
-% Given the functor of a predicate (of the form name/arity), return the
-% negation.
+%!  negate_functor(+Functor:compound, -NegFunctor:compound) is det
 %
-% @arg Functor The functor of a predicate.
-% @arg NegFunctor The negated functor.
+%   Given the functor of a predicate   (of  the form name/arity), return
+%   the negation.
+%
+%   @arg  Functor The functor of a predicate.
+%   @arg  NegFunctor The negated functor.
+
 negate_functor(F, N) :-
     atom_chars(F, Fc),
     atom_chars('n_', Fn), % negation prefix
@@ -113,13 +121,16 @@ negate_functor(F, N) :-
     atom_chars(N, N2),
     !.
 
-%! is_atom(+Goal:compound, +Vars:list, -Value:atom) is det
-% Given a goal, succeed if it is an atom or a variable bound to an atom. Check
-% vars first, since internal representation of a variable is also an atom.
+%!  is_atom(+Goal:compound, +Vars:list, -Value:atom) is det
 %
-% @arg Goal The input goal.
-% @arg Vars The list of variable values and constraints.
-% @arg Value The atom.
+%   Given a goal, succeed if it is  an   atom  or a variable bound to an
+%   atom. Check vars first, since internal  representation of a variable
+%   is also an atom.
+%
+%   @arg  Goal The input goal.
+%   @arg  Vars The list of variable values and constraints.
+%   @arg  Value The atom.
+
 is_atom(G, V, A) :-
     is_var(G),
     !,
@@ -130,13 +141,15 @@ is_atom(G, _, G) :-
     !,
     G =.. [G | []].
 
-%! is_compound(+Goal:compound, +Vars:list, -Value:compound) is det
-% Given a goal, succeed if it is a compound term or a variable bound to a
-% compound term.
+%!  is_compound(+Goal:compound, +Vars:list, -Value:compound) is det
 %
-% @arg Goal The input goal.
-% @arg Vars The list of variable values and constraints.
-% @arg Value The compound term.
+%   Given a goal, succeed if it is a   compound term or a variable bound
+%   to a compound term.
+%
+%   @arg  Goal The input goal.
+%   @arg  Vars The list of variable values and constraints.
+%   @arg  Value The compound term.
+
 is_compound(G, V, A) :-
     is_var(G),
     !,
@@ -148,23 +161,25 @@ is_compound(G, _, G) :-
     G =.. [_ | T],
     T \= []. % not an atom
 
-%! is_dual(+Functor:atom) is semidet.
+%!  is_dual(+Functor:atom) is semidet.
 %
-%  Succeed if a functor contains the prefix '_not_', indicating that
-%  it's a dual.
+%   Succeed if a functor contains the   prefix  '_not_', indicating that
+%   it's a dual.
 %
-%  @arg Functor The functor to test.
+%   @arg Functor The functor to test.
+
 is_dual(X) :-
     has_prefix(X, n).
 
-%! split_functor(+Functor:atom, -Name:list, -Arity:int) is det.
+%!  split_functor(+Functor:atom, -Name:list, -Arity:int) is det.
 %
-%  Given a predicate functor, return the  components. Since the arity is
-%  at the end, we have to be creative to remove it.
+%   Given a predicate functor, return the components. Since the arity is
+%   at the end, we have to be creative to remove it.
 %
-%  @arg Functor The predicate functor, of the form Name/Arity.
-%  @arg Name The name with the arity stripped. A list of characters.
-%  @arg Arity The arity of the predicate, or -1 if no arity is attached.
+%   @arg Functor The predicate functor, of the form Name/Arity.
+%   @arg Name The name with the arity stripped. A list of characters.
+%   @arg Arity The arity of the predicate, or -1 if no arity is
+%        attached.
 
 split_functor(P, N, A) :-
     sub_atom(P, Plen, _, Slen, '_'),
@@ -178,15 +193,17 @@ split_functor(P, N, -1) :- % no arity attached
     atom_chars(P, N),
     !.
 
-%! create_unique_functor(+Head:ground, +Counter:int, -NewHead:ground) is det
-% Create a unique functor by inserting the counter characters just before the
-% /arity.
+%!  create_unique_functor(+Head:ground, +Counter:int, -NewHead:ground) is det
 %
-% @arg Head A functor of the form head/arity to form the base of the unique
+%   Create a unique functor by  inserting   the  counter characters just
+%   before the /arity.
+%
+%   @arg  Head A functor of the form head/arity to form the base of the unique
 %        functor.
-% @arg Counter Counter to ensure that the functor is unique. Don't reuse it
+%   @arg  Counter Counter to ensure that the functor is unique. Don't reuse it
 %        with the same base.
-% @arg DualHead The functor for the dual of an individual clause.
+%   @arg  DualHead The functor for the dual of an individual clause.
+
 create_unique_functor(Hi, C, Ho) :-
     split_functor(Hi, Fc, A), % Strip the arity
     number_chars(C, Cc),
@@ -197,57 +214,65 @@ create_unique_functor(Hi, C, Ho) :-
     atom_chars(Ho, Fc3),
     !.
 
-%! fatal_error(+Format:string, +Arguments:list) is det
-% Call write_error/2 and then halt.
+%!  fatal_error(+Format:string, +Arguments:list) is det
 %
-% @arg Format A quoted string that would be passed to writef/2. '~w' specifies
+%   Call write_error/2 and then halt.
+%
+%   @arg  Format A quoted string that would be passed to writef/2. '~w' specifies
 %        that the next element in Arguments should be printed.
-% @arg Arguments A list of arguments that will be substituted, in order, for
+%   @arg  Arguments A list of arguments that will be substituted, in order, for
 %        '~w' in Format.
+
 fatal_error(X, Y) :-
     write(user_error, 'FATAL '), % add to start of error message
     write_error(X, Y),
     halt(1).
 
-%! write_error(+Format:string) is det
-% Write a formatted error message to the user_error stream. Since no writef
-% predicate allows writing to streams, first write to a string, then write the
-% string to the stream. NOTE: Syntax and parsing errors are handled differently.
-% See common_dcg:syntax_error/3, tokenizer:eof_error and tokenizer:lex_error/2.
+%!  write_error(+Format:string) is det
 %
-% @arg Format Anything that could be passed to write/1: a quoted string, a
+%   Write a formatted error message to   the user_error stream. Since no
+%   writef predicate allows writing to streams, first write to a string,
+%   then write the string to the stream. NOTE: Syntax and parsing errors
+%   are    handled    differently.     See    common_dcg:syntax_error/3,
+%   tokenizer:eof_error and tokenizer:lex_error/2.
+%
+%   @arg  Format Anything that could be passed to write/1: a quoted string, a
 %        term, etc.
 
 write_error(X) :-
     split_string(X, "", "\n", [String]),
     print_message(error, format('~w',String)).
 
-%! write_error(+Format:string, +Arguments:list) is det
-% Similar to write_error/1, except that arguments are filled in as with
-% writef/2.
+%!  write_error(+Format:string, +Arguments:list) is det
 %
-% @arg Format A quoted string that would be passed to writef/2. '~w' specifies
+%   Similar to write_error/1, except that  arguments   are  filled in as
+%   with writef/2.
+%
+%   @arg  Format A quoted string that would be passed to writef/2. '~w' specifies
 %        that the next element in Arguments should be printed.
-% @arg Arguments A list of arguments that will be substituted, in order, for
+%   @arg  Arguments A list of arguments that will be substituted, in order, for
 %        '~w' in Format.
 
 write_error(X, Y) :-
     split_string(X, "", "\n", [Fmt]),
     print_message(error, format(Fmt,Y)).
 
-%! write_verbose(+Depth:int, +Format:string) is det.
-% Write a message to user_error if the verbose or veryverbose options are
-% enabled (by passing -v or -vv as command-line parameters). The first argument
-% is an integer representing the "depth" of the message. This is used to indent
-% messages for readability and determine which messages to print for each mode.
-% Top-level messages should use depth 0, with each subsequent level increasing
-% the depth by 1. Verbose mode will print only those messages with a depth of 0,
-% while veryverbose mode will print all messages. Unlike write_error/1 and
-% write_error/2, no prefix or terminal newline are appended.
+%!  write_verbose(+Depth:int, +Format:string) is det.
 %
-% @arg Depth The indentation level of the message.
-% @arg Format The item to print. This will probably be a quoted string, but
+%   Write a message to user_error if  the verbose or veryverbose options
+%   are enabled (by passing -v or   -vv as command-line parameters). The
+%   first argument is  an  integer  representing   the  "depth"  of  the
+%   message. This is  used  to  indent   messages  for  readability  and
+%   determine which messages to print for  each mode. Top-level messages
+%   should use depth 0, with each  subsequent level increasing the depth
+%   by 1. Verbose mode will print only those messages with a depth of 0,
+%   while veryverbose mode will print all messages. Unlike write_error/1
+%   and write_error/2, no prefix or terminal newline are appended.
+%
+%   @arg Depth The indentation level of the message.
+%   @arg Format The item to print. This will probably be a quoted string, but
 %        can be anything accepted by write/1.
+
 write_verbose(0, Y) :-
     user_option(verbose, 1),
     write(user_error, Y),
@@ -260,15 +285,17 @@ write_verbose(X, Y) :-
 write_verbose(_, _) :-
     !.
 
-%! write_verbose(+Depth:int, +Format:string, +Arguments:list) is det
-% Similar to write_verbose/2, except that arguments are filled in as with
-% writef/2.
+%!  write_verbose(+Depth:int, +Format:string, +Arguments:list) is det
 %
-% @arg Depth The indentation level of the message.
-% @arg Format A quoted string in which '~w' will be replaced by the next item
+%   Similar to write_verbose/2, except that arguments   are filled in as
+%   with writef/2.
+%
+%   @arg  Depth The indentation level of the message.
+%   @arg  Format A quoted string in which '~w' will be replaced by the next item
 %        in Arguments.
-% @arg Arguments A list of arguments that will be substituted, in order, for
+%   @arg  Arguments A list of arguments that will be substituted, in order, for
 %        '~w' in Format.
+
 write_verbose(0, Y, Z) :-
     user_option(verbose, 1),
     !,
@@ -280,27 +307,27 @@ write_verbose(X, Y, Z) :-
     format(user_error, Y, Z).
 write_verbose(_, _, _).
 
-%! write_indent(+Depth:int)
-% Write an indentation of size Depth to user_error.
+%!  write_indent(+Depth:int)
 %
-% @arg Depth The number of indentations to print. Note that a single
+%   Write an indentation of size Depth to user_error.
+%
+%   @arg Depth The number of indentations to print. Note that a single
 %        indentation need not equate to a single space.
-write_indent(0).
-write_indent(X) :-
-    X > 0,
-    write(user_error, '  '),
-    X2 is X - 1,
-    write_indent(X2).
 
-%! var_list(+N:int, +Count:int, +ListIn:list, -ListOut:list) is det
-% Get a list of N variables, each of which is different. Basically, just append
-% a counter to '_X'. The '_' prefix ensures they don't overlap with any existing
-% variables in a rule.
+write_indent(Depth) :-
+    tab(user_error, 2*Depth).
+
+%!  var_list(+N:int, +Count:int, +ListIn:list, -ListOut:list) is det
 %
-% @arg N The size of the list to return.
-% @arg Count Counter to ensure that each variable has a unique name.
-% @arg ListIn Input list.
-% @arg ListOut output list.
+%   Get a list of N variables, each   of  which is different. Basically,
+%   just append a counter to '_X'.  The   '_'  prefix ensures they don't
+%   overlap with any existing variables in a rule.
+%
+%   @arg  N The size of the list to return.
+%   @arg  Count Counter to ensure that each variable has a unique name.
+%   @arg  ListIn Input list.
+%   @arg  ListOut output list.
+
 var_list(N, C, Li, Lo) :-
     N > 0,
     !,
@@ -314,12 +341,14 @@ var_list(0, _, L, L2) :-
     reverse(L, L2), % restore order
     !.
 
-%! list_diff(+ListA:list, +ListB:list, -ListC:list) is det
-% Get all of the members of ListA that are not members of ListB.
+%!  list_diff(+ListA:list, +ListB:list, -ListC:list) is det
 %
-% @arg ListA The first input list.
-% @arg ListB The second input list.
-% @arg ListC The output list.
+%   Get all of the members of ListA that are not members of ListB.
+%
+%   @arg  ListA The first input list.
+%   @arg  ListB The second input list.
+%   @arg  ListC The output list.
+
 list_diff([X | T], Y, Z) :-
     member(X, Y),
     !,
@@ -330,12 +359,14 @@ list_diff([X | T], Y, [X | Z]) :-
 list_diff([], _, []) :-
     !.
 
-%! list_intersection(+ListA:list, +ListB:list, -ListC:list) is det
-% Get all of the members of ListA that are also members of ListB.
+%!  list_intersection(+ListA:list, +ListB:list, -ListC:list) is det
 %
-% @arg ListA The first input list.
-% @arg ListB The second input list.
-% @arg ListC The output list.
+%   Get all of the members of ListA that are also members of ListB.
+%
+%   @arg ListA The first input list.
+%   @arg ListB The second input list.
+%   @arg ListC The output list.
+
 list_intersection([X | T], Y, [X | Z]) :-
     member(X, Y),
     !,
@@ -346,14 +377,16 @@ list_intersection([_ | T], Y, Z) :-
 list_intersection([], _, []) :-
     !.
 
-%! operator(+Operator:ground, -Specifier:atom, -Priority:int) is det
-% ASP / Prolog operator table. Original table from the ISO Prolog standard, with
-% unsupported operators removed. NOTE: Some of the operators below may not have
-% been implemented yet.
+%!  operator(+Operator:ground, -Specifier:atom, -Priority:int) is det
 %
-% @arg Operator An arithmetic operator.
-% @arg Specifier Defines associativity of operator.
-% @arg Priority Defines operator priority.
+%   ASP/Prolog operator table.  Original  table   from  the  ISO  Prolog
+%   standard, with unsupported operators  removed.   NOTE:  Some  of the
+%   operators below may not have been implemented yet.
+%
+%   @arg Operator An arithmetic operator.
+%   @arg Specifier Defines associativity of operator.
+%   @arg Priority Defines operator priority.
+
 operator(',', xfy, 1000).
 operator(=, xfx, 700).
 operator(\=, xfx, 700).
