59350af4b419c14be727aedde575cd210aa88c27
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-07-12 08:54:32 +0200

    More refector rules

--- src/refactor.pl
+++ src/refactor.pl
@@ -31,14 +31,33 @@ replace_lpdoc(Options) :-
                      Replace,
                      replace_doc(Type, Text, Replace),
                      Options),
-    replace_sentence((:- use_package(_)),
+    replace_sentence((:- use_package(assertions)),
                      '$NODOT'('$TEXT'("")),
                      true,
                      Options),
     replace_sentence((:- use_module(library(CiaoLib))),
                      Replace,
                      ciao_lib(CiaoLib, Replace),
+                     Options),
+    replace_sentence((:- expects_dialect(ciao)),
+                     '$NODOT'('$TEXT'("")),
+                     true,
+                     Options),
+    replace_sentence((:- data(Preds)),
+                     (:- dynamic(Preds)),
+                     true,
                      Options).
+
+replace_goals :-
+    forall(project_file(File),
+           replace_goals([file(File)])).
+
+replace_goals(Options) :-
+    replace_goal(if(A,B,C), (A*->B,C), true, Options),
+    replace_goal(num(X), number(X), true, Options),
+    replace_goal(struct(X), compound(X), true, Options),
+    replace_goal(varset(T,Vs), term_variables(T,Vs), true, Options),
+    replace_goal(insert(S0,E,S), ord_add_element(S0,E,S), true, Options).
 
 ciao_lib(CiaoLib, Replace) :-
     ciao_swi_lib(CiaoLib, SWILib),
@@ -47,7 +66,13 @@ ciao_lib(CiaoLib, Replace) :-
     ;   Replace = (:- use_module(library(SWILib)))
     ).
 
+ciao_swi_lib(write, []).
 ciao_swi_lib(terms_vars, []).
+ciao_swi_lib(formulae, []).
+ciao_swi_lib(dict, []).
+ciao_swi_lib(stream_utils, []).
+ciao_swi_lib(sets, []).
+ciao_swi_lib(terms_check, []).
 
 
 lpdoc2pldoc(Head : _ # Comment, Dict, PlDoc) =>
@@ -115,6 +140,10 @@ make_part(pred, Pred, _, Part) =>
     Part = Pred.
 make_part(em, Content, _, Part) =>
     Part = ['_', Content, '_'].
+make_part(bf, Content, _, Part) =>
+    Part = ['__', Content, '__'].
+make_part(file, Content, _, Part) =>
+    Part = ['``', Content, '``'].
 make_part(Cmd, Content, _, Part) =>
     print_message(warning, lpdoc(unknown_command(Cmd))),
     Part = [@, Cmd, '{', Content, '}'].
@@ -152,12 +181,13 @@ replace_doc(module, Comment, Replace) =>
     lpdoc2markdown(Comment, [], MarkDown, [width(72)]),
     doc_data(title, Title),
     doc_data(author, Author),
-    format(string(Replace),
+    format(string(Header),
            '/** <module> ~w~n~n\c
            ~w~n~n\c
            @author ~w~n\c
            */~n~n',
-           [ Title, MarkDown, Author ]).
+           [ Title, MarkDown, Author ]),
+    Replace = '$NODOT'('$TEXT'(Header)).
 
 section_header(Title, SeqHdr) :-
     with_output_to(string(SeqHdr), section_header(Title)).
