76b4280dad3a7f3bcf9f32451a1b1634c63733e2
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-07-16 09:35:21 +0200

    Comments, restructuring, dead code removal.

--- src/clp_disequality.pl
+++ src/clp_disequality.pl
@@ -1,7 +1,6 @@
 :- module(clp_disequality,
           [ get_neg_var/2,
             not_unify/2,
-            loop_list/2,
             (.=.)/2,
             (.\=.)/2,
             loop_term/2,
@@ -9,6 +8,8 @@
             op(700, xfx, .=.),
             op(700, xfx, .\=.)
           ]).
+:- use_module(scasp_io).
+:- use_module(clp_clpq).
 
 /** <module> Constraint solver for disequalities
 
@@ -22,24 +23,19 @@ disequality.
 @author Joaquin Arias
 */
 
-:- use_module(scasp_io).
-
 :- dynamic disunify/2.
 
 
 		 /*******************************
-		 *       MAIN PREDICATES        *
+		 *   CONSTRUCTIVE UNIFICATION   *
 		 *******************************/
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Constructive Unification %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% - Constructive unification of a negatively constrained variable
-%% with a non- variable value will succeed if the non-variable value
-%% does not constructively unify with any element in the variable’s
-%% prohibited value list.
+%!  A .=. B
+%
+%   Constructive unification of a negatively constrained variable with a
+%   non- variable value will succeed if  the non-variable value does not
+%   constructively unify with any element   in the variable’s prohibited
+%   value list.
 
 :- if(\+((dif(A,b), unifiable(a(A), a(b), _)))).
 :- format(user_error, 'ERROR: unifiable/3 validates constraints~n', []),
@@ -56,33 +52,36 @@ unify([Var=Value|T]) :-
     unify(T).
 
 unify2(A, B) :-
-    nonvar(B), !,
+    nonvar(B),
+    !,
     (   get_neg_var(A,NegListA)
     ->  not_unify(B, NegListA),
         clean(A)
     ;   true
     ),
     A = B.
-%% - Constructive unification of two negatively constrained variables
-%% will always succeed, setting their shared prohibited value list to
-%% the union of their original lists.
+% - Constructive unification of two negatively constrained variables
+% will always succeed, setting their shared prohibited value list to
+% the union of their original lists.
 unify2(A, B) :-
     get_neg_var_or_empty(A,NegListA),
-    get_neg_var_or_empty(B,NegListB), !,
+    get_neg_var_or_empty(B,NegListB),
+    !,
     ord_union(NegListA,NegListB,NegList),
     update(A,NegList),
     clean(B),
     B = A.
-%% - In cases where neither argument contains a negatively constrained
-%% variable, the result is identical to that of traditional
-%% unification.
+% - In cases where neither argument contains a negatively constrained
+% variable, the result is identical to that of traditional
+% unification.
 unify2(A,B) :-
     A = B.
 
+		 /*******************************
+		 *  CONSTRUCTIVE DISUNIFICATION	*
+		 *******************************/
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Constructive disunification %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%!  A .\=. B
 
 .\=.(A,B) :-
     (   unifiable(A,B,U0)
@@ -92,9 +91,9 @@ unify2(A,B) :-
     ;   true
     ).
 
-%% - in accordance with the restrictions given in Section 3.1.5,
-%% constructive disunification of two non ground variables will
-%% produce an error
+% - in accordance with the restrictions given in Section 3.1.5,
+% constructive disunification of two non ground variables will
+% produce an error
 not_unify2(_,B) :-
     var(B),
     !,
@@ -102,55 +101,27 @@ not_unify2(_,B) :-
 not_unify2(A, B) :-
     is_clpq_var(A), !,
     disequality_clpq(A,B).
-%% - Constructive disunification of a negatively constrained variable
-%% and a non- variable value will always succeed, adding the
-%% "ground" value to the variable’s prohibited value list.
-not_unify2(A,B) :-
+% - Constructive disunification of a negatively constrained variable
+% and a non- variable value will always succeed, adding the
+% "ground" value to the variable’s prohibited value list.
+not_unify2(A, B) :-
     ground(B),
-    neg_var(A,NegListA),
-    ord_add_element(NegListA,B,NegList),
-    update(A,NegList).
+    neg_var(A, NegListA),
+    ord_add_element(NegListA, B, NegList),
+    update(A, NegList).
 
-loop_var_disequality(A,B) :-
-    neg_var(A,ListA),
-    neg_var(B,ListB),
-    ListA == [],
-    ListB \== [],
-    loop_var_disequality_(A,ListB).
-loop_var_disequality(B,A) :-
-    neg_var(A,ListA),
-    neg_var(B,ListB),
-    ListA == [],
-    ListB \== [],
-    loop_var_disequality_(A,ListB).
-loop_var_disequality(A,B) :-
-    if_user_option(check_calls, format('\t\tLoop_var_disequality( ~p , ~p )\n',[A,B])),
-    A .\=. B.
-
-loop_var_disequality_(A,[NegB|_]) :-
-    A .=. NegB.
-loop_var_disequality_(A, [_|NegBs]) :-
-    loop_var_disequality_(A, NegBs).
-
-
+%!  not_unify(+Term, +List)
+%
+%   True when Term cannot unify with any of the elements in List
 
 not_unify(_A, []) :- !.
 not_unify(A, [X|Xs]) :-
     A .\=. X,
     not_unify(A,Xs).
 
-
-
-:- use_module(clp_clpq).
-loop_list([A|As],[B|Bs]) :-
-    (
-        loop_var_disequality(A,B)
-    ;
-    %     loop_var_clpq(A,B)
-    % ;
-        A .=. B,
-        loop_list(As,Bs)
-    ).
+%!  loop_term(+Goal1, +Goal2)
+%
+%
 
 loop_term(Goal1, Goal2) :-
     functor(Goal1, Name, Arity),
@@ -167,6 +138,30 @@ loop_term(I, Arity, Goal1, Goal2) :-
         I2 is I+1,
         loop_term(I2, Arity, Goal1, Goal2)
     ).
+
+
+%!  loop_var_disequality(?A, ?B)
+%
+%   ???
+
+loop_var_disequality(A, B) :-
+    neg_var(A, ListA),
+    neg_var(B, ListB),
+    (   ListA == [],
+        ListB \== []
+    ->  loop_var_disequality_(A, ListB)
+    ;   ListB == [],
+        ListA \== []
+    ->  loop_var_disequality_(B, ListA)
+    ).
+loop_var_disequality(A, B) :-
+    if_user_option(check_calls, format('\t\tLoop_var_disequality( ~p , ~p )\n',[A,B])),
+    A .\=. B.
+
+loop_var_disequality_(A, [NegB|_]) :-
+    A .=. NegB.
+loop_var_disequality_(A, [_|NegBs]) :-
+    loop_var_disequality_(A, NegBs).
 
 
 %%%%%%%%%%%%%%%%%%%%%%
@@ -232,25 +227,20 @@ entail_neg_list(L1, L2) :-
 %       add(Join,NegListJoin).
 
 
-%% ------------------------------------------------------------- %%
 		 /*******************************
 		 *     AUXILIAR PREDICATES      *
 		 *******************************/
 
-
-%% Auxiliar predicates %%
-neg_var(A,List) :-
-    (
-        get_attr(A,clp_disequality,neg(List)), true ->
-        true
-    ;
-        var(A),
-        List = [],
-        put_attr(A,clp_disequality,neg(List))
+neg_var(A, List) :-
+    (   get_attr(A, clp_disequality, neg(List))
+    ->  true
+    ;   var(A)
+    ->  List = [],
+        put_attr(A, clp_disequality, neg(List))
     ).
 
 get_neg_var(A,List) :-
-    get_attr(A,clp_disequality,neg(List)).
+    get_attr(A, clp_disequality, neg(List)).
 
 get_neg_var_or_empty(A,List) :-
     var(A),
@@ -259,65 +249,19 @@ get_neg_var_or_empty(A,List) :-
     ;   List = []
     ).
 
+clean(A) :-
+    del_attr(A, clp_disequality).
 
-unbound(A) :-
-    var(A),
-    (
-        get_attr(A,clp_disequality,neg(List)), true ->
-        List == []
-    ;
-        true
-    ).
-clean(A) :- del_attr(A, clp_disequality).
-update(A,List) :- put_attr(A,clp_disequality,neg(List)).
-add(A,Value) :-
-    (
-        neg_var(A,NegListA), true ->
-        ord_add_element(NegListA, Value, NegList),
-        update(A,NegList)
-    ;
-        put_attr(A,clp_disequality,neg([Value]))
-    ).
-dump_neg_list(A,neg(List)) :-
-    get_attr(A,clp_disequality,neg(List)).
+update(A,List) :-
+    put_attr(A,clp_disequality,neg(List)).
 
-
-:- multifile attr_unify_hook/2, attribute_goals/3, attr_portray_hook/2.
 attr_unify_hook(neg(A),B) :-
-    (
-        not_unify(B,A) ->
-        true
-    ;
-        % print('Fail unification between:  '),
-        % print(B),print('  and the neg list '), print(A),nl,
-        fail
-    ).
+    not_unify(B, A).
 
 attribute_goals(X) -->
-    [.\=.(X, G)],
-     {get_attr(X,clp_disequality,neg(G))}.
+    { get_neg_var(X, G)
+    },
+    [.\=.(X, G)].
+
 attr_portray_hook(neg(List), Var) :-
-    format("~w.\\=.~w",[Var,List]).
-
-:- multifile portray_attribute/2.
-portray_attribute(att(_,false,att(clp_disequality,neg(List),_)),Var) :-
-    (
-        List == [] ->
-        display(Var)
-    ;
-        format(" {~w ∉ ~w} ",[Var,List])
-    ).
-
-%% Auxiliar predicates %%
-
-%% ------------------------------------------------------------- %%
-
-%% Not needed %%
-insert_more_general([A|As],B,[A|As]) :-
-    entail(A,B), !.
-insert_more_general([A|As],B,[B|As]) :-
-    entail(B,A), !.
-insert_more_general([A|As],B,[A|Rs]) :-
-    insert_more_general(As,B,Rs).
-insert_more_general([],B,[B]).
-%% Not needed %%
+    format("~p .\\=. ~p",[Var,List]).
