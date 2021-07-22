a4c07e0295238f65334c5fad6d798e7897aff03f
Xuaco <joaquin.arias@urjc.es>
2021-07-22 11:49:00 +0200

    Swi version

--- src/scasp_options.pl
+++ src/scasp_options.pl
@@ -22,7 +22,7 @@
 %   print the current version of s(CASP)
 
 scasp_version :-
-    format('s(CASP) version ~p\n',['0.21.05.28']),
+    format('s(CASP) version ~p\n',['swi.0.21.07.22']),
     halt.
 
 set_options(Options) :-
