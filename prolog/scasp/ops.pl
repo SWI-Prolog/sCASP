01357775d712e2f7fe0586a113dd06b377073631
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-08-11 17:55:35 +0200

    Allow local definition of sCASP operators.

--- prolog/scasp/ops.pl
+++ prolog/scasp/ops.pl
@@ -5,5 +5,30 @@
             op(700, xfx, ['| ']),
             op(700, xfx, [~>, <~]),
             op(900, fy,  [not]),
-            op(700, xfx, ['\u2209'])
+            op(700, xfx, ['\u2209']),
+
+            scasp_push_operators/0,
+            scasp_pop_operators/0
           ]).
+:- use_module(library(apply)).
+:- use_module(library(operators)).
+
+scasp_push_operators :-
+    prolog_load_context(module, Context),
+    findall(op(Pri, Ass, Op), scasp_op(Pri, Ass, Op), Ops),
+    push_operators(Context:Ops).
+scasp_pop_operators :-
+    pop_operators.
+
+term_expansion(scasp_op(Pri, Ass, List), Ops) :-
+    maplist(mkop(Pri, Ass), List, Ops).
+
+mkop(Pri, Ass, Op, scasp_op(Pri, Ass, Op)).
+
+scasp_op(700, xfx, [.=., .\=.,.<>.,.<.,.=<.,.>.,.>=.]).
+scasp_op(700, xfx, [#= , #<>, #< , #> , #=<, #>= ]).
+scasp_op(700, xfx, [::]).
+scasp_op(700, xfx, ['| ']).
+scasp_op(700, xfx, [~>, <~]).
+scasp_op(900, fy,  [not]).
+scasp_op(700, xfx, ['\u2209']).
