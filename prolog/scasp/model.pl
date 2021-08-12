cbb490107ef8d44dd3ba8ba2d2c7b6438abe2dea
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-08-12 17:22:00 +0200

    Simplification and sorting of the model

--- prolog/scasp/model.pl
+++ prolog/scasp/model.pl
@@ -36,6 +36,8 @@
           ]).
 
 /** <module> sCASP model handling
+
+
 */
 
 %!  canonical_model(+RawModel, -Canonical)
@@ -43,7 +45,68 @@
 canonical_model(Model, CanModel) :-
     flatten(Model, FlatModel),
     exclude(nonmodel_term, FlatModel, Model1),
-    sort(Model1, CanModel).
+    sort(Model1, Unique),
+    maplist(raise_negation, Unique, Raised),
+    sort_model(Raised, Sorted),
+    simplify_model(Sorted, CanModel).
 
 nonmodel_term(proved(_)).
+nonmodel_term(chs(_)).
 nonmodel_term(o_nmr_check).
+
+raise_negation(not(TermIn), not(Term)) :-
+    !,
+    raise_negation(TermIn, Term).
+raise_negation(TermIn, -Term) :-
+    functor(TermIn, Name, _),
+    atom_concat(-, Plain, Name),
+    !,
+    TermIn =.. [Name|Args],
+    Term   =.. [Plain|Args].
+raise_negation(Term, Term).
+
+%!  sort_model(+ModelIn, -Sorted) is det.
+%
+%   Sort the model by literal, getting   affirming and denying knowledge
+%   about a literal together.  If multiple terms about the same literal
+%   appear they are ordered:
+%
+%     1. positive
+%     2. not(positive)
+%     3. -positive
+%     4. not(-positive).
+
+sort_model(ModelIn, Sorted) :-
+    map_list_to_pairs(literal_key, ModelIn, Pairs),
+    keysort(Pairs, KSorted),
+    pairs_values(KSorted, Sorted).
+
+literal_key(Term, Literal-Flags) :-
+    literal_key(Term, Literal, 0, Flags).
+
+literal_key(not(X), Key, F0, F) =>
+    literal_key(X, Key, F0, F1),
+    F is F1+0x1.
+literal_key(-(X), Key, F0, F) =>
+    literal_key(X, Key, F0, F1),
+    F is F1+0x2.
+literal_key(X, Key, F0, F) =>
+    Key = X,
+    F = F0.
+
+%!  simplify_model(+ModelIn, -ModelOut) is det.
+%
+%   Remove model terms that are entailed by other model terms.
+
+simplify_model([not(X0),-X1|T0], M), X0 == X1 =>
+    M = [-X1|M0],
+    simplify_model(T0, M0).
+simplify_model([X0, not(-(X1))|T0], M), X0 == X1 =>
+    M = [X0|M0],
+    simplify_model(T0, M0).
+simplify_model([H|T0], M) =>
+    M = [H|M0],
+    simplify_model(T0, M0).
+simplify_model([], M) =>
+    M = [].
+
