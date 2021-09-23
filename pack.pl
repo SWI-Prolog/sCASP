4e416639a38a1504bb97ee6c67c756ddda4311b6
Jan Wielemaker <J.Wielemaker@vu.nl>
2021-09-23 11:35:29 +0200

    Turn into a pack

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(scasp).
+version('0.9.0').
+title('Goal directed ASP solver').
+keywords([scasp, asp]).
+author( 'Joaquin Arias', 'joaquin.arias@urjc.es' ).
+author( 'Kyle Marple', '' ).
+author( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
+home('https://github.com/JanWielemaker/sCASP' ).
+download( 'https://github.com/JanWielemaker/sCASP/archive/*.zip' ).
