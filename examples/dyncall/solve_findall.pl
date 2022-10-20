24f07955c0e8522b248a2a1fde5f9b3cc1056fec
Jan Wielemaker <J.Wielemaker@vu.nl>
2022-10-20 10:55:24 +0200

    Added dynamic calling vesion of solve_findall.pl

---
+++ examples/dyncall/solve_findall.pl
@@ -0,0 +1,10 @@
+:- use_module(library(scasp)).
+
+validate(L) :-
+    findall(X, member(X,L), LL),
+    LL = L.
+
+member(X1, [X2|_]) :- X1 #= X2.
+member(X, [_|R]) :- member(X, R).
+
+% ?- validate([1,2,3]).
