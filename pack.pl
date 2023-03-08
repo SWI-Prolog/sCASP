603122fa8ceaf0aea58f4bd1f796112087e37f03
Jan Wielemaker <J.Wielemaker@vu.nl>
2023-03-08 16:53:59 +0100

    Move to git for downloads.

--- pack.pl
+++ pack.pl
@@ -5,5 +5,5 @@ keywords([scasp, asp]).
 author( 'Joaquin Arias', 'joaquin.arias@urjc.es' ).
 author( 'Kyle Marple', '' ).
 author( 'Jan Wielemaker', 'jan@swi-prolog.org' ).
-home('https://github.com/JanWielemaker/sCASP' ).
-download( 'https://github.com/JanWielemaker/sCASP/archive/*.zip' ).
+home('https://github.com/SWI-Prolog/sCASP' ).
+download( 'https://github.com/SWI-Prolog/sCASP.git' ).
